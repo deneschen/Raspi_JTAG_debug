b449ad54aff426e9bcfdec36d4c93507164ffa44
Chenjun <chenjun675816156@gmail.com>
2020-04-20 01:04:17 +0800

    openocd for windows

---
+++ openocd/debug_raspi4.bat
@@ -0,0 +1,3 @@
+::bin\openocd.exe -f share\openocd\scripts\interface\jlink.cfg -f share\openocd\scripts\target\raspberrypi3.cfg
+bin\openocd.exe -f share\openocd\scripts\interface\jlink.cfg -f raspi4.cfg
+pause
